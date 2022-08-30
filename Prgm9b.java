b2ecbe8fd727e065fc3984dcb2e84b80701125ea
Badri FF13 <rbadri7501@gmail.com>
2022-08-30 16:22:35 +0530

    solved-9a ignore-7b

--- Prgm9b.java
+++ Prgm9b.java
@@ -1,97 +1,33 @@
-#include<iostream>
-
-using namespace std;
-
-string grades(int marks )  
-
+import java.util.Scanner;
+public class Prgm9b
 {
-
- 
-
-   if(marks<=100 && marks>=91)
-
-{
-
- cout<<"The grade of a student is AA.";
-
- 
-
-}
-
-else if(marks<=90 && marks>=81)
-
-{
-
- cout<<"The grade of a student is AB.";
-
-}
-
-else if(marks<=80 && marks>=71)
-
-{
-
- cout<<"The grade of a student is BB.";
-
-}
-
-else if(marks<=70 && marks>=61)
-
-{
-
- cout<<"The grade of student is BC.";
-
-}
-
-else if(marks<=60 && marks>=51)
-
-{
-
- cout<<"The grade of a student is CD.";
-
-}
-
-else if(marks<=50 && marks>=41)
-
-{
-
- cout<<"The grade of a student is DD.";
-
-}
-
-else if(marks<=40)
-
-{
-
- cout<<"The student is fail.";
-
-}
-
-else
-
-{
-
- cout<<" Bye Bye";
-
-}
-
-}
-
-int main()
-
-{
-
-int marks;
-
-cout<<" Enter the marks(0-100) of a student:";
-
-cin>>marks;
-
-cout<<grades(marks);
-
- 
-
-return 0;
-
- 
-
+    public static String calcGrades(int grade){
+        if(grade>90)
+            return "AA";
+        else if(grade>80)
+            return "AB";
+        else if(grade>70)
+            return "BB";
+        else if(grade>60)
+            return "BC";
+        else if(grade>50)
+            return "CD";
+        else if(grade>40)
+            return "DD";
+        else
+            return "Fail";
+    }
+    public static void main(String[] args){
+        System.out.println("Enter the Number of Students");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the Grade of Student "+(i+1));
+            marks[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.println("Student "+(i+1)+" Grade is "+calcGrades(marks[i]));
+        }
+    }
 }
